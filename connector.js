36eb02afed47dc877f1c5f6addf6b035ee67f7d5
Lbayu <bayufathlanip@gmail.com>
2025-04-28 08:45:45 +0700

    Initial commit

---
+++ connector.js
@@ -0,0 +1,50 @@
+(function() {
+    var myConnector = tableau.makeConnector();
+  
+    myConnector.getSchema = function(schemaCallback) {
+      var cols = [
+        { id: "name", dataType: tableau.dataTypeEnum.string },
+        { id: "age", dataType: tableau.dataTypeEnum.int },
+        { id: "city", dataType: tableau.dataTypeEnum.string }
+      ];
+  
+      var tableSchema = {
+        id: "dataTable",
+        alias: "Sample data from Google Sheets",
+        columns: cols
+      };
+  
+      schemaCallback([tableSchema]);
+    };
+  
+    myConnector.getData = function(table, doneCallback) {
+      var url = document.getElementById("https://docs.google.com/spreadsheets/d/e/2PACX-1vRf7iQivW3eFpE1V083ddaaMSYN3TfSv2CwX6hQokHWEtsyIgZrRejc1YQvl_gUjaZssXhOkGDO4AlM/pub?output=csv").value;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          var tableData = [];
+          data.forEach(item => {
+            tableData.push({
+              "name": item.name,
+              "age": item.age,
+              "city": item.city
+            });
+          });
+  
+          table.appendRows(tableData);
+          doneCallback();
+        })
+        .catch(error => {
+          document.getElementById("status").innerText = "Error fetching data: " + error;
+          doneCallback();
+        });
+    };
+  
+    tableau.registerConnector(myConnector);
+  
+    document.getElementById("fetchButton").addEventListener("click", function() {
+      tableau.connectionName = "Google Sheets Data";
+      tableau.submit();
+    });
+  })();
+  
